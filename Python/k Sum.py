b48eb79773451dcf45947b2f6bea6901c3e2129c
Anthony <anthonyjin0619@gmail.com>
2015-11-15 17:32:07 -0800

    add 2d DP for K SUm

--- Python/k Sum.py
+++ Python/k Sum.py
@@ -34,3 +34,24 @@ class Solution:
                         dp[i][j][n] += dp[i - 1][j - 1][n - A[i - 1]]
                     dp[i][j][n] += dp[i - 1][j][n]
         return dp[-1][-1][-1]
+
+class Solution:
+    """
+    @param A: An integer array.
+    @param k: a positive integer (k <= length(A))
+    @param target: integer
+    @return an integer
+    """
+    def kSum(self, A, k, target):
+        # write your code here
+        length = len(A)
+        dp = [[0 for _ in range(target + 1)] for _ in range(k + 1)]
+
+        for i in range(1, length + 1):
+            for j in reversed(range(1, min(k, i) + 1)):
+                for n in reversed(range(A[i - 1], target + 1)):
+                    if j == 1:
+                        dp[j][n] += 1 if n == A[i - 1] else 0
+                    else:
+                        dp[j][n] += dp[j - 1][n - A[i - 1]]
+        return dp[-1][-1]
